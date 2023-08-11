595b2ea09a79844a10761bf2d9df527a5858aad4
mmertaltayy <mertaltayy_@outlook.com>
2023-08-11 03:00:31 +0300

    Butonların tanımlamaları yapıldı

--- Hesap Makines/script.js
+++ Hesap Makines/script.js
@@ -15,5 +15,44 @@ keys.addEventListener('click', function(e){
 
     if(!element.matches('button')) return;
 
-    console.log(element);
-})
+    if(element.classList.contains('operator')) {
+        console.log('operator', element.value);
+         return;
+    }
+
+    if(element.classList.contains('decimal')) {
+        // console.log('decimal', element.value);
+        inputDecimal();
+        updateDisplay();
+         return;
+    }
+
+    if(element.classList.contains('clear')) {
+        // console.log('clear', element.value);
+        clear();
+        updateDisplay();
+         return;
+    }
+
+    // console.log('number', element.value);
+    inputNumber(element.value);
+    updateDisplay();
+
+});
+
+function inputNumber(num) {
+    displayValue = displayValue === '0'? num: displayValue + num;
+
+}
+
+function inputDecimal() {
+    if (!displayValue.includes('.')) {
+        displayValue += '.';
+    }
+}
+
+function clear() {
+    displayValue = '0'
+
+}
+
