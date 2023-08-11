b766100fe1cf29fc7f635a958613fc8a1a51f1a3
mmertaltayy <mertaltayy_@outlook.com>
2023-08-11 15:52:57 +0300

    Hesaplamalar Yapıdı.

--- Hesap Makines/script.js
+++ Hesap Makines/script.js
@@ -2,6 +2,9 @@ const display = document.querySelector(".calculator-input");
 const keys = document.querySelector(".calculator-keys");
 
 let displayValue = "0";
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
 
 
 updateDisplay();
@@ -16,7 +19,9 @@ keys.addEventListener('click', function(e){
     if(!element.matches('button')) return;
 
     if(element.classList.contains('operator')) {
-        console.log('operator', element.value);
+        // console.log('operator', element.value);
+        handleOperator(element.value);
+        updateDisplay();
          return;
     }
 
@@ -40,8 +45,54 @@ keys.addEventListener('click', function(e){
 
 });
 
+function handleOperator(nextOperator) {
+    const value = parseFloat(displayValue);
+    if(operator && waitingForSecondValue) {
+        operator = nextOperator;
+        return;
+    }
+
+    if (firstValue === null) {
+        firstValue = value;
+    } else if (operator){
+        const result = calculate(firstValue, value, operator);
+
+        displayValue = `${parseFloat(result.toFixed(7))}`;
+        firstValue = result;
+
+    }
+    waitingForSecondValue = true;
+    operator = nextOperator;
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
+
+}
+
+function calculate(first, second, operator) {
+    if (operator === '+') {
+        return first + second;
+    } else if (operator === '-'){
+        return first - second;
+    } else if (operator ==='*') {
+        return first * second;
+    }else if (operator ==='/') {
+        return first / second;
+    }
+    return second;
+}
+
 function inputNumber(num) {
-    displayValue = displayValue === '0'? num: displayValue + num;
+    if(waitingForSecondValue) {
+        displayValue= num;
+        waitingForSecondValue = false;
+    }else {
+
+        displayValue = displayValue === '0'? num: displayValue + num;
+
+    }
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
+    
 
 }
 
